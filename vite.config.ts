ec0cd3a0231b93924868104d147cf708414c1a77
TheAdeogo <aadeogo@gmail.com>
2026-02-06 21:50:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // This allows the app to read the API_KEY you set in Vercel
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   }
 });
